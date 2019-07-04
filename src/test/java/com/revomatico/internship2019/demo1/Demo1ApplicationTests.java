4fc014067f6b98d3026cc7ea7b50d7bfe18ca37c
raisercostin <raisercostin@gmail.com>
2019-07-04 18:12:57 +0300

    first spring hello world app

---
+++ src/test/java/com/revomatico/internship2019/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revomatico.internship2019.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
