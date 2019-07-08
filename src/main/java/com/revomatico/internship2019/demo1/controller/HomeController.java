7300231439c1a7641ad3c4594cc2cf601df24336
IordacheDario99 <dario.iordache@gmail.com>
2019-07-08 21:27:59 +0300

    Working HelloWord web-application

---
+++ src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.revomatico.internship2019.demo1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+}
