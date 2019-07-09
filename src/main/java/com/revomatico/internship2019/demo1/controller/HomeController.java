796e6b730aefc607b0f8a2c0081a4f793c48f208
raisercostin <raisercostin@gmail.com>
2019-07-09 11:44:58 +0300

    refactoring

--- src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
+++ src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
@@ -5,8 +5,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping("/")
-    public String home(){
-        return "Hello World!";
+  @RequestMapping("/")
+  public String home() {
+    return "Events:<br/>" + table();
+  }
+
+  private String table() {
+    String result = "";
+    for (int i = 0; i < 10; i++) {
+      result += row();
     }
+    String tagName = "table";
+    return "<" + tagName + ">" + result + "</" + tagName + ">";
+  }
+
+  private String row() {
+    // TODO d
+    // return null;
+    // throw new RuntimeException("Not implemented yet!!!");
+    String tagName = "tr";
+    String tagName2 = "td";
+    return "<" + tagName + ">" + "<" + tagName2 + ">" 
+        + "rand" + "</" + tagName2 + ">" + "</" + tagName + ">";
+    // "<tr><td>rand</td></tr>";
+  }
 }
