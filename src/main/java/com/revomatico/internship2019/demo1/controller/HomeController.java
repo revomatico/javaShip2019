28e28541d60babe7af013c1db58b1fa8f09b63ec
raisercostin <raisercostin@gmail.com>
2019-07-09 13:13:43 +0300

    fixed

--- src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
+++ src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.revomatico.internship2019.demo1.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import io.vavr.collection.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,21 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
   @RequestMapping("/")
-  public String home() {
+  public String home() throws FileNotFoundException {
     return displayEvents(readEvents());
   }
 
   private String displayEvents(List<List<String>> rows) {
-    String displayedRows = rows.map(row -> row(row.get(0), row.get(1)+"bla")).mkString();
+    String displayedRows = rows.map(row -> row(row.get(0), row.get(1))).mkString();
     String table = tag("table", tag("tr", tag("th", "name") + tag("th", "date")) + displayedRows);
     return "Events:<br/>" + table;
   }
 
-  private List<List<String>> readEvents() {
-    List<List<String>> rows = List.of(List.of("concert rock", "2019-07-09 18:00"), List.of("concert rock2", "2019-07-09 18:00"),
-        List.of("concert rock3", "2019-07-09 18:00"), List.of("concert rock4", "2019-07-09 18:00"),
-        List.of("concert rock5", "2019-07-09 18:00"));
+  private List<List<String>> readEvents() throws FileNotFoundException {
+    List<List<String>> rows = List.empty();
+    Scanner sc = new Scanner(new File("d:\\home\\raiser\\work\\internship\\java-internship-2019\\src\\main\\resources\\events.csv"));
+    while(sc.hasNext()) {
+      String line = sc.nextLine();
+      String[] vals = line.split(",");
+      rows = rows.append(List.of(vals[0],vals[1]));
+    }
     return rows;
+    // List<List<String>> rows = List.of(List.of("concert rock", "2019-07-09 18:00"), List.of("concert rock2",
+    // "2019-07-09 18:00"),
+    // List.of("concert rock3", "2019-07-09 18:00"), List.of("concert rock4", "2019-07-09 18:00"),
+    // List.of("concert rock5", "2019-07-09 18:00"));
+    // return rows;
   }
 
   private String tag(String string, String result) {
