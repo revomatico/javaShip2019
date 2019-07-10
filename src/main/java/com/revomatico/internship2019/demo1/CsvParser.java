296dff40e94fa4837f65aed6146c95210fb03268
Danutz99 <danmamaliga2@gmail.com>
2019-07-10 21:45:28 +0300

    opencsv depency used

---
+++ src/main/java/com/revomatico/internship2019/demo1/CsvParser.java
@@ -0,0 +1,56 @@
+package com.revomatico.internship2019.demo1;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.opencsv.CSVReader;
+
+import io.vavr.collection.List;
+
+public class CsvParser {
+    private static final String Path = "src\\main\\resources\\events.csv";
+    
+    public static Map<String,String> initRestrictedKeywords(){
+    	Map<String,String> restrictedKeywords = new HashMap<>();
+    	restrictedKeywords.put("\"","");
+    	restrictedKeywords.put(","," ");
+    	
+    	return restrictedKeywords;
+    }
+    
+    public static String filterKeyWord(String token,Map<String,String> restrictedKeys) {
+	
+    	for(Map.Entry<String, String> element : restrictedKeys.entrySet()) {
+    		if(token.contains(element.getKey())) {
+    			token = token.replace(element.getKey(), element.getValue());
+    		}
+    	}
+    	
+    	return token;
+    }
+    
+
+    public static List<List<String>> ReadCsv() throws IOException {
+    	List<List<String>> rows = List.empty();
+    	Map<String,String> restrictedKeywords = initRestrictedKeywords();
+       
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(Path));
+            CSVReader csvReader = new CSVReader(reader);
+        ) {csvReader.readNext();
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                String firstRecord = filterKeyWord(nextRecord[0],restrictedKeywords);
+                rows = rows.append(List.of(firstRecord, nextRecord[1]));
+            }
+        	
+            return rows;
+        }
+        
+    }
+}
--- src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
+++ src/main/java/com/revomatico/internship2019/demo1/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.revomatico.internship2019.demo1.controller;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import io.vavr.collection.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revomatico.internship2019.demo1.CsvParser;
 
 @RestController
 public class HomeController {
@@ -21,16 +24,33 @@ public class HomeController {
     return "Events:<br/>" + table;
   }
 
-  private List<List<String>> readEvents() throws FileNotFoundException {
-    List<List<String>> rows = List.empty();
-    try (Scanner sc = new Scanner(new File("src\\main\\resources\\events.csv"))) {
-      while (sc.hasNext()) {
-        String line = sc.nextLine();
-        String[] vals = line.split(",");
-        rows = rows.append(List.of(vals[0], vals[1]));
-      }
-    }
-    return rows;
+ private List<List<String>> readEvents() throws FileNotFoundException {
+   List<List<String>> rows = List.empty();
+   try {
+	rows = CsvParser.ReadCsv();
+
+} catch (IOException e) {
+	e.printStackTrace();
+}
+//  try (Scanner sc = new Scanner(new File("src\\main\\resources\\events.csv"))) {
+//   while (sc.hasNext()) {
+//      String line = sc.nextLine();
+//       
+//       String[] vals = line.split(",");
+//       if(vals[0].startsWith("\"")) {
+//       	line = line.replace("\"", "");
+//       	vals = line.split(",");
+//           rows = rows.append(List.of(vals[0] + " " +vals[1], vals[2]));
+//       }else {
+//       	vals = line.split(",");
+//            rows = rows.append(List.of(vals[0] ,vals[1] ));
+//        }
+//
+// 
+//     }
+//   }
+   
+   return rows;
     // List<List<String>> rows = List.of(List.of("concert rock", "2019-07-09 18:00"), List.of("concert rock2",
     // "2019-07-09 18:00"),
     // List.of("concert rock3", "2019-07-09 18:00"), List.of("concert rock4", "2019-07-09 18:00"),
