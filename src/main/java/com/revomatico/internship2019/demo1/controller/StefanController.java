c49b52f7625e4af243e14d4e789eeed1a31c4e70
Stefan Lazea <lazeastefan@gmail.com>
2019-07-11 01:25:37 +0300

    Added implementation for the readEvents using commons csv

---
+++ src/main/java/com/revomatico/internship2019/demo1/controller/StefanController.java
@@ -0,0 +1,67 @@
+package com.revomatico.internship2019.demo1.controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.vavr.collection.List;
+
+@RestController
+public class StefanController {
+  @RequestMapping("/custom")
+  public String home() throws IOException {
+    return displayEvents(readEvents());
+  }
+
+  private String displayEvents(List<List<String>> rows) {
+    String displayedRows = rows.map(row -> row(row.get(0), row.get(1))).mkString();
+    String table = tag("table", tag("tr", tag("th", "name") + tag("th", "date")) + displayedRows);
+    return "Events custom:<br/>" + table;
+  }
+
+  private List<List<String>> readEvents() throws IOException {
+    List<List<String>> rows = List.empty();
+        
+    try (
+    		BufferedReader reader = new BufferedReader(new FileReader("src\\main\\resources\\events.csv"));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader()
+                    .withIgnoreHeaderCase()
+                    .withTrim());
+        ) {
+            for (CSVRecord csvRecord : csvParser) {
+               rows = rows.append(List.of(csvRecord.get("name"), csvRecord.get("date")));
+
+            }
+        }
+      return rows;
+    
+  }
+
+  private String tag(String string, String result) {
+    return tagAsHtml(string, result);
+  }
+
+  private String tagAsHtml(String tagName, String result) {
+    return "<" + tagName + ">" + result + "</" + tagName + ">";
+  }
+
+  private String tagAsJson(String tagName, String result) {
+    return "{\"" + tagName + "\":\"" + result + "\"}";
+  }
+
+  private String row(String name, String date) {
+    return tag("tr", tag("td", name) + tag("td", date));
+  }
+}
