97221f92391afca98a1e96853d384831d0258f88
correndonomundo <andra.stancu@gmail.com>
2019-07-15 17:03:01 +0300

    some H2 tests

---
+++ src/main/java/com/revomatico/internship2019/demo1/modelCuHibernate/EventSpringBootApp.java
@@ -0,0 +1,52 @@
+package com.revomatico.internship2019.demo1.modelCuHibernate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@SpringBootApplication
+@ComponentScan("package com.revomatico.internship2019.demo1.readers.event")
+public class ClientSpringBootApp {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        System.setProperty("spring.datasource.url","jdbc:h2:mem:testdb");
+        SpringApplication.run(ClientSpringBootApp.class, args);
+    }
+
+    @PostConstruct
+    private void initDb() {
+        String sqlStatements[] = {
+                "insert into test(NAME, DATE) values('Concert5','20-iul-2019')",
+                "insert into test(NAME, DATE) values('Concert6','30-iul-2019')"
+        };
+
+        Arrays.asList(sqlStatements).stream().forEach(sql -> {
+            System.out.println(sql);
+            jdbcTemplate.execute(sql);
+        });
+
+
+        jdbcTemplate.query("select id,NAME,DATE from test",
+                new RowMapper<Object>() {
+                    @Override
+                    public Object mapRow(ResultSet rs, int i) throws SQLException {
+                        System.out.println(String.format("id:%s,NAME:%s,DATE:%s",
+                                rs.getString("id"),
+                                rs.getString("NAME"),
+                                rs.getString("DATE")));
+                        return null;
+                    }
+                });
+    }
+}
---
+++ src/main/java/com/revomatico/internship2019/demo1/modelCuHibernate/SpringBootH2Application.java
@@ -0,0 +1,4 @@
+package com.revomatico.internship2019.demo1.modelCuHibernate;
+
+public class SpringBootH2Application {
+}
